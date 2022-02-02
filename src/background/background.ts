f2fbaeb91e82d6466a43fb9ac44bf557a5f2d378
Martijn <martijncvv@gmail.com>
2022-02-02 15:04:46 +0000

    style: cleaned unused code

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,17 +3,6 @@ import { setStoredCoinList, setStoredCoins } from '../utils/storage'
 
 console.log('BACKGROUND script is running')
 
-chrome.runtime.onInstalled.addListener(async () => {
-	for (const cs of chrome.runtime.getManifest().content_scripts) {
-		for (const tab of await chrome.tabs.query({ url: cs.matches })) {
-			chrome.scripting.executeScript({
-				target: { tabId: tab.id },
-				files: cs.js,
-			})
-		}
-	}
-})
-
 fetchCoinsList().then((data) => {
 	setStoredCoinList(data)
 	console.log('Coins fetched')
